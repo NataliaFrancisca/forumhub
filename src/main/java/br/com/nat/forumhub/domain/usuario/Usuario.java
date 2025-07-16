9b97b0b9d5c4ebdcd2481a6ffb6814ece821bf03
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-16 00:06:43 -0300

    feat: criando arquivos necessários para a entidade Usuario

---
+++ src/main/java/br/com/nat/forumhub/domain/usuario/Usuario.java
@@ -0,0 +1,84 @@
+package br.com.nat.forumhub.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    private Perfil perfil;
+
+    public Usuario(UsuarioRegistro usuario){
+        this.email = usuario.email();
+        this.senha = usuario.senha();
+        this.nome = usuario.nome();
+        this.perfil = usuario.perfil();
+    }
+
+    public void atualizar(UsuarioAtualizado usuario){
+        if (usuario.nome() != null){
+            this.nome = usuario.nome();
+        }
+
+        if (usuario.senha() != null){
+            this.senha = usuario.senha();
+        }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.perfil.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
