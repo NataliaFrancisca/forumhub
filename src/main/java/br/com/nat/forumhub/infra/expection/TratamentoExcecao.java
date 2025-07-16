064951a2988029449840ee552bb2173bb54586a0
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-16 00:08:41 -0300

    feat: criando arquivo global para tratar as exceções

---
+++ src/main/java/br/com/nat/forumhub/infra/expection/TratamentoExcecao.java
@@ -0,0 +1,71 @@
+package br.com.nat.forumhub.infra.expection;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratamentoExcecao {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception){
+        var erros = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(
+                erros.stream()
+                        .map(DadosErroValidacao::new)
+                        .toList()
+        );
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity tratarErroDeViolacaoDados(Exception ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado(Exception ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado: " + ex.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro500(){
+        return ResponseEntity.badRequest().build();
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(),erro.getDefaultMessage());
+        }
+    }
+}
