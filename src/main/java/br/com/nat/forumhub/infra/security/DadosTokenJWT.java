1fc0b43a015ac65c3bab33a7d326041262443149
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-16 00:06:14 -0300

    feat: criando os arquivos necessários para configurar o spring-security e assim garantir segurança nas rotas e autenticação e autorização.

---
+++ src/main/java/br/com/nat/forumhub/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package br.com.nat.forumhub.infra.security;
+
+public record DadosTokenJWT(String token){
+}
---
+++ src/main/java/br/com/nat/forumhub/infra/security/SecurityFilter.java
@@ -0,0 +1,65 @@
+package br.com.nat.forumhub.infra.security;
+
+import br.com.nat.forumhub.domain.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = this.recuperarToken(request);
+
+        if (tokenJWT == null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try{
+            var subject = this.tokenService.getSubject(tokenJWT);
+            var usuario = this.usuarioRepository.findByEmail(subject);
+            var autenticacao = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(autenticacao);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write(
+                    """
+                       {
+                        "erro": "Token JWT inválido ou expirado. Por favor, faça login novamente."
+                       }
+                    """
+            );
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request){
+        var header = request.getHeader("Authorization");
+
+        if (header != null){
+            return header.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
