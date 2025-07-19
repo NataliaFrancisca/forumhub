8d94464232b6704949569ba47ba66305d7b6958d
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-18 23:54:14 -0300

    feat: adicionando nova propriedade e anotações de validações para cada propriedade.

--- src/main/java/br/com/nat/forumhub/domain/resposta/RespostaRegistro.java
+++ src/main/java/br/com/nat/forumhub/domain/resposta/RespostaRegistro.java
@@ -1,7 +1,14 @@
 package br.com.nat.forumhub.domain.resposta;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record RespostaRegistro(
+        @NotBlank(message = "O campo mensagem é obrigatório.")
         String mensagem,
-        String solucao
+        @NotBlank(message = "O campo solução é obrigatório.")
+        String solucao,
+        @NotNull(message = "O campo de identificação do tópico é obrigatório.")
+        Long topicoId
 ) {
 }
